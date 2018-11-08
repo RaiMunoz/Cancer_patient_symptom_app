55d1b747da98a2a50f103ef481bf0105b800c3a6
liz14zyt <liz14zyt@gmail.com>
2018-11-07 23:10:01 -0500

    added tutorial for keeping track of state and moving screens

---
+++ vita/lib/home_widget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'placeholder_widget.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    PlaceholderWidget(Colors.white),
+    PlaceholderWidget(Colors.deepOrange),
+    PlaceholderWidget(Colors.green),
+    PlaceholderWidget(Colors.purple),
+    PlaceholderWidget(Colors.brown),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Flutter App'),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.book),
+            title: new Text('Diary'),
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.add_circle_outline),
+            title: new Text('Add'),
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.insert_chart),
+            title: new Text('Analytics'),
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.message),
+            title: new Text('Message'),
+            backgroundColor: Colors.blue,
+          )
+        ],
+      ),
+    );
+  }
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
