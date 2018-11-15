3d012bf2babbb21e6327ac234615562889131475
lbressler13 <lbressler13@gmail.com>
2018-11-14 22:43:53 -0500

    Update severity on screen when dropdown value is selected

--- vita/lib/pages/diary/diary_symptoms.dart
+++ vita/lib/pages/diary/diary_symptoms.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 import 'dart:async';
 import 'package:intl/intl.dart';
 
-import 'diary_entry.dart';
+import 'symptom_entry.dart';
 import 'contact_service.dart';
 
 
@@ -16,52 +16,63 @@ class diary_symptoms extends StatefulWidget {
 }
 
 class _diary_symptoms extends State<diary_symptoms> {
-  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> formKey = new GlobalKey<FormState>();
   List<int> severity_list = <int>[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   int severity = null;
+  symptom_entry entry = new symptom_entry();
+
+ /* void submit_form() {
+    final FormState form = formKey.currentState;
+
+    form.save();
+    var contactService = new ContactServiceSymptom();
+    contactService.createSymptomEntry(entry);
+  }*/
 
   @override
-    Widget build(BuildContext context) {
-      return new Scaffold(
-        appBar: AppBar(),
-        body: new ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-          children: <Widget>[
-            new Column(children: <Widget>[
-            new TextFormField(
-              decoration: const InputDecoration(
-                icon: const Icon(Icons.content_paste),
-                hintText: 'What symptom are you experiencing?',
-              ),
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      key: scaffoldKey,
+      appBar: AppBar(),
+      body: new ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+        children: <Widget>[
+          new Column(children: <Widget>[
+          new TextFormField(
+            decoration: const InputDecoration(
+              icon: const Icon(Icons.content_paste),
+              hintText: 'What symptom are you experiencing?',
             ),
-            /*new DropdownButton<int>(
-              value: severity,
-              items: severity_list.map<DropdownMenuItem<int>>((int value) {
-                return DropdownMenuItem<int>(
-                  value: value,
-                  child: Text(value.toString()),
-                );
-              }).toList(),
-            ),*/
-            new DropdownButton<int>(
-              items: severity_list.map((int value) {
-                return new DropdownMenuItem<int>(
-                  value: value,
-                  child: new Text(value.toString()),
-                );
-              }).toList(),
-              onChanged: (_) {},
-            ),
-            new Container(
-              padding: EdgeInsets.only(left: 40.0, top: 20.0),
-              child: new RaisedButton(
-                child: const Text('Submit'),
-                onPressed: () {},
-              )
-            ),
-            ]),
-          ],
-        ),
-      );
-    }
+            onSaved: (val) => entry.symptom_name = val,
+          ),
+          new DropdownButton<int>(
+            value: severity,
+            items: severity_list.map((int value) {
+              return new DropdownMenuItem<int>(
+                value: value,
+                child: new Text(value.toString()),
+              );
+            }).toList(),
+            onChanged: (val) {
+              setState(() {
+                entry.severity = val;
+                severity = val;
+                //state.didChange(val);
+              });
+            }
+    //onSaved: (val) => entry.severity = val,
+    ),
+    new Container(
+    padding: EdgeInsets.only(left: 40.0, top: 20.0),
+            child: new RaisedButton(
+              child: const Text('Submit'),
+              onPressed: () {},
+            )
+          ),
+          ]),
+        ],
+      ),
+    );
   }
+}
---
+++ vita/lib/pages/diary/symptom_entry.dart
@@ -0,0 +1,5 @@
+class symptom_entry {
+  String symptom_name;
+  int severity;
+  DateTime record_time;
+}
