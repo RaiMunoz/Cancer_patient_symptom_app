81a92cb68c439610685300bf17864e39ad6bf0b5
lbressler13 <lbressler13@gmail.com>
2018-11-18 21:39:25 -0500

    Use ExpansionTile for symptom button (previous was FlatButton)

--- vita/lib/pages/diary/symptoms/symptom_button.dart
+++ vita/lib/pages/diary/symptoms/symptom_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../assets/theme/theme.dart';
+
 class symptom_button extends StatefulWidget {
   final String title;
   final int severity;
@@ -16,67 +18,32 @@ class symptom_rating_button extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new FlatButton(
+    return new RaisedButton(
       child: Text(
         rating.toString(),
         style: TextStyle(color: Colors.white),
       ),
-      shape: CircleBorder(),
+      //shape: CircleBorder(side: BorderSide(),),
+      color: ThemeColors.darkGreen,
       onPressed: () {},
     );
   }
 }
 
 class _symptom_button extends State<symptom_button> {
-
   @override
-  Widget build(BuildContext context) {
-    return Container (
+  Widget build(BuildContext) {
+    return Container(
       width: MediaQuery.of(context).size.width * 0.8,
-      height: MediaQuery.of(context).size.height * 0.2,
-      child: FlatButton(
-        child: Container (
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget> [
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      widget.title,
-                      style: TextStyle(color: Colors.white)
-                    ),
-                  ),
-                  IconButton(
-                    icon: Icon(
-                      Icons.add_circle,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-              FittedBox(
-                child: Visibility(
-                  child: Row(
-                    children: <Widget>[
-                      symptom_rating_button(1),
-                      symptom_rating_button(2),
-                      symptom_rating_button(2),
-                      symptom_rating_button(2),
-                      symptom_rating_button(2),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-        color: Colors.lightGreen,
-        onPressed: (){},
+      child: ExpansionTile(
+        title: Text(widget.title),
+        children: <Widget>[
+          symptom_rating_button(1),
+          symptom_rating_button(1),
+          symptom_rating_button(1),
+          symptom_rating_button(2),
+          symptom_rating_button(2),
+        ],
       ),
     );
   }
