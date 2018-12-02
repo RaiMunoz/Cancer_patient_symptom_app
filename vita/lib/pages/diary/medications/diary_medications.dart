437423bdf30d62115008d7c67f20564d8ded1af9
lbressler13 <lbressler13@gmail.com>
2018-12-02 06:04:27 -0500

    Submit all medications at same time

--- vita/lib/pages/diary/medications/diary_medications.dart
+++ vita/lib/pages/diary/medications/diary_medications.dart
@@ -12,30 +12,53 @@ class diary_medications extends StatefulWidget {
 }
 
 class _diary_medications extends State<diary_medications> {
+  List<medication> meds = [
+    medication(
+        name: 'hi',
+        dose: '15mg',
+        started: DateTime.now(),
+        stopped: null,
+        current: true,
+        frequency: 0,
+        times: [],
+        notifications: false
+    ),
+  ];
+
+  List<medication_button> all_medications = <medication_button>[];
+
+  void submit_all_medications() {
+    for(int i = 0; i < all_medications.length; i++) {
+      all_medications[i].submitForm();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<medication> meds = [
-      medication(
-          name: 'hi',
-          dose: '15mg',
-          started: DateTime.now(),
-          stopped: null,
-          current: true,
-          frequency: 0,
-          times: [],
-          notifications: false
-      ),
-    ];
+    if(all_medications.length == 0) {
+      for(int i = 0; i < meds.length; i++) {
+        all_medications.add(medication_button(
+          title: meds[i].name,
+          med: meds[i],
+        ));
+      }
+    }
 
     return new Scaffold(
       appBar: AppBar(
         title: Text('My Medications'),
         centerTitle: true,
+        leading: IconButton(
+          icon: BackButtonIcon(),
+          onPressed: () {
+            submit_all_medications();
+            Navigator.maybePop(context);
+          }
+        ),
       ),
       body: ListView.builder(
-        itemBuilder:
-            (context, int i) {
-          return medication_button(title: meds[i].name, med: meds[i]);
+        itemBuilder: (context, int i) {
+          return all_medications[i];
         },
         itemCount: meds.length,
         padding: EdgeInsets.symmetric(
--- vita/lib/pages/diary/medications/medication_button.dart
+++ vita/lib/pages/diary/medications/medication_button.dart
@@ -15,23 +15,17 @@ import '../entry_button_generic.dart';
 class medication_button extends StatefulWidget {
   final String title;
   final medication med;
+  final GlobalKey<FormState> formKey = new GlobalKey<FormState>();
+  final entry = new medication_entry();
 
   const medication_button({Key key, this.title, this.med}): super(key: key);
 
-  @override
-  _medication_button createState() => new _medication_button();
-}
-
-class _medication_button extends State<medication_button> {
-  final GlobalKey<FormState> formKey = new GlobalKey<FormState>();
-  int severity = null;
-  medication_entry entry = new medication_entry();
-
   void submitForm() {
+    print('here');
     final FormState form = formKey.currentState;
 
     form.save();
-    entry.medication_name = widget.title;
+    entry.medication_name = title;
     if(entry.dosage != null && entry.dosage != '' && entry.time_taken != null) {
       var contactService = new ContactServiceMedication();
       contactService.createMedicationEntry(entry);
@@ -42,18 +36,43 @@ class _medication_button extends State<medication_button> {
   }
 
   @override
+  _medication_button createState() => new _medication_button();
+}
+
+class _medication_button extends State<medication_button> {
+  int severity = null;
+
+  /*void submitForm() {
+    final FormState form = widget.formKey.currentState;
+
+    form.save();
+    entry.medication_name = widget.title;
+    if(entry.dosage != null && entry.dosage != '' && entry.time_taken != null) {
+      var contactService = new ContactServiceMedication();
+      contactService.createMedicationEntry(entry);
+      print('Created entry: \nMedication name: ' + entry.medication_name +
+          '\nDosage: ' + entry.dosage +
+          '\nTime taken: ' + DateFormat.yMd().add_jm().format(entry.time_taken));
+    }
+  }*/
+
+  @override
   Widget build(BuildContext) {
+    if(widget.entry.dosage == null) {
+      widget.entry.dosage = widget.med.dose;
+    }
+
     return Form(
-      key: formKey,
+      key: widget.formKey,
       child: entry_button_generic(
         title: entry_title(widget.med.name),
         children: <Widget>[
           FittedBox(
-            child: select_dose_time(entry: entry, med: widget.med),
+            child: select_dose_time(entry: widget.entry, med: widget.med),
           ),
         ],
         action: (expanded) {
-          if(!expanded) submitForm();
+          //if(!expanded) this.submitForm();
         },
       ),
     );
